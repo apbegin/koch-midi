bf5ea2428cdaacbedf5907d27763330c982b9e6e
apbegin <apbegin@gmail.com>
2016-02-03 14:48:15 -0500

    ajout de troncature de la distance entre les points

--- fractale.py
+++ fractale.py
@@ -4,31 +4,34 @@ __author__ = 'JJ'
 
 import math
 
+
+
+
 def _dist(p1, p2):
 #Fonction _distX(x, y)
 # qui calcul la distance horizontale entre deux points de
 #   coordonees (x,y)
 #RETOURNE un nombre
-    dist_x_squared = math.pow((p2[0]-p1[0]), 2) #dist entre x de p2 et p1
-    dist_y_squared = math.pow((p2[1]-p1[1]), 2) #dist entre y de p2 et p1
+    dist_x_squared = (p2[0]-p1[0])**2 #dist entre x de p2 et p1
+    dist_y_squared = (p2[1]-p1[1])**2 #dist entre y de p2 et p1
 
-    return math.sqrt(dist_x_squared + dist_y_squared)
+    return trunc(math.sqrt(dist_x_squared + dist_y_squared))
 
 def _fraction_distance(p1, p2, fraction):
     #Fonction qui retourne les coordonnees d'un point se trouvant a une
     #certaine fraction de la distance entre deux points
     vect_points = [p2[0]-p1[0], p2[1]-p1[1]]   #Vecteur de distantce entre p1 et p2
-    new_point = [p1[0] + fraction*vect_points[0],
-                 p1[1] + fraction*vect_points[1]]   #Nouveau point = p1 + fraction distance p2-1
+    new_point = [p1[0] + fraction * vect_points[0],
+                 p1[1] + fraction * vect_points[1]]   #Nouveau point = p1 + fraction distance p2-1
     return new_point
 
 
-def trunc(n):
-#fonction  h =trunc( 48/2n ) est utilisee pour calculer h de la recursion
+def trunct(n):
+#fonction  h =trunct( 48/2n ) est utilisee pour calculer h de la recursion
 # n est le niveau de recursion de la fonction musique
 #RETOURNE un array de longueur 2
-    hauteur = 48/(math.pow(2, n))
-    hauteur = math.floor(hauteur)
+    hauteur = trunc(48/(2**n))
+    
     return hauteur
 
 def _genere_points(p1, p2, n):
@@ -36,7 +39,7 @@ def _genere_points(p1, p2, n):
     #a faire un fractal de koch [p1--p3--p4--p5--p2} OU [A--C--D--E--B}
     p3 = _fraction_distance(p1, p2, 1/4)    #point C (ou p3) est sur la la droite AB
     p4 = _fraction_distance(p1, p2, 2/4)    #point D (ou p5) est sur la la droite AB
-    p4[1] = trunc(n)                        #ajuste la hauteur de D selon trunct
+    p4[1] = trunct(n)                        #ajuste la hauteur de D selon trunct
     p5 = _fraction_distance(p1, p2, 3/4)    #point E (ou p5) est sur la la droite AB
     return [p1, p3, p4, p5, p2]
 
