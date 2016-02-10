a99189c6154f5bdd070283fa89bb68ff20f701ed
apbegin <apbegin@gmail.com>
2016-02-09 20:16:15 -0500

    correction de la generation des notes et du temps

--- fractale.py
+++ fractale.py
@@ -1,106 +1,95 @@
+__author__ = 'Jonathan Jackson & Antoine Proulx-Bégin'
+
 from math import trunc
-from _ast import List
 import struct
-__author__ = 'JJ'
 
-
-import math
-
-
-
-def _dist(p1, p2):
+def __dist(p1, p2):
 #Fonction _distX(x, y)
 # qui calcul la distance horizontale entre deux points de
 #   coordonees (x,y)
 #RETOURNE un nombre
-    dist_x_squared = (p2[0]-p1[0])**2 #dist entre x de p2 et p1
-    dist_y_squared = (p2[1]-p1[1])**2 #dist entre y de p2 et p1
+    return (p2[0] - p1[0])
 
-    return math.sqrt(dist_x_squared + dist_y_squared)
-
-def _fraction_distance(p1, p2, fraction):
+def __fraction_distance(p1, p2, fraction):
     #Fonction qui retourne les coordonnees d'un point se trouvant a une
     #certaine fraction de la distance entre deux points
     vect_points = [p2[0]-p1[0], p2[1]-p1[1]]   #Vecteur de distantce entre p1 et p2
     new_point = [p1[0] + fraction * vect_points[0],
                  p1[1] + fraction * vect_points[1]]   #Nouveau point = p1 + fraction distance p2-1
+    
     return new_point
 
-
-
-
-def _genere_points(p1, p2, n):
+def __genere_points(p1, p2, n):
     #fonction qui prend deux points et retourne 4 points selon de sorte
     #a faire un fractal de koch [p1--p3--p4--p5--p2} OU [A--C--D--E--B}
-    p3 = _fraction_distance(p1, p2, 1/4)    #point C (ou p3) est sur la la droite AB
-    p4 = _fraction_distance(p1, p2, 2/4)    #point D (ou p5) est sur la la droite AB
+    p3 = __fraction_distance(p1, p2, 1/3)    #point C (ou p3) est sur la la droite AB
+    p4 = __fraction_distance(p1, p2, 1/2)    #point D (ou p5) est sur la la droite AB
     p4[1] = trunc(p4[1] + 48/(2**n))                        #ajuste la hauteur de D selon trunct
-    p5 = _fraction_distance(p1, p2, 3/4)    #point E (ou p5) est sur la la droite AB
+    p5 = __fraction_distance(p1, p2, 2/3)    #point E (ou p5) est sur la la droite AB
     return [p1, p3, p4, p5, p2]
-
-def musique(A, B, niveau, niveauMax):
+ 
+def __musique(A, B, niveau, niveauMax):
     n = niveau
     
-    next_points = _genere_points(A, B, n)
+    next_points = __genere_points(A, B, n)
     if niveau == niveauMax:
-        return [round(A[1]), (round(_dist(A, B)))] #on retourne la hauteur de A et la dist AB
+        return [round(A[1]), (round(__dist(A, B)))] #on retourne la hauteur de A et la dist AB
     else:
         n += 1
-        return [] + musique(next_points[0], next_points[1], n, niveauMax)+\
-               musique(next_points[1], next_points[2], n, niveauMax) +\
-               musique(next_points[2], next_points[3], n, niveauMax) +\
-               musique(next_points[3], next_points[4], n, niveauMax)
-               
+        return [] + __musique(next_points[0], next_points[1], n, niveauMax)+\
+               __musique(next_points[1], next_points[2], n, niveauMax) +\
+               __musique(next_points[2], next_points[3], n, niveauMax) +\
+               __musique(next_points[3], next_points[4], n, niveauMax)
 
-#Ecriture du hearder dans le fichier midi
+def __generer_midi(koch_values):
 
-
+    lst_note = []
+    channel = 0x90
+    volume = 0x60
+    
+    dist_index = trunc(len(koch_values)/2)
+    
+    #determine le temps d'un évenement  
+    for x in range(dist_index-1):
+        encoding_time = 0x00
+        if koch_values[2*x+1] <= 127:
+            encoding_time=0x80
+            
+        elif koch_values[2*x+1] <= 254:
+            encoding_time=0x81
+            koch_values[2*x+1]=koch_values[2*x+1]-127
+            
+        elif koch_values[2*x+1] >254:
+            
+            koch_values[2*x+1]=koch_values[2*x+1]-254
+            encoding_time = 0x82
+            
+        lst_note.append([encoding_time,koch_values[2*x+1],channel,koch_values[2*x],volume])
+        
+    #Section G du track header :
+    #nb de note x 5 pour le nombre de byte + 4 byte de la section F 
+    track_length = len(lst_note)*5 + 4
+    
+    midiHeader =[0x4d, 0x54,0x68, 0x64 ,0x00, 0x00, 0x00,0x06, 0x00,
+                0x00, 0x00, 0x01, 0x00, 0x80, 0x4d, 0x54, 0x72, 0x6b]
+    
+    f = open('musique.mid','wb')
+    f.truncate()
+    
+    f.write(bytearray(midiHeader))
+    f.write(struct.pack('>I',track_length))
+    
+    for x in range(len(lst_note)):
+        track_buffer = bytearray(5)
+        struct.pack_into('>5B',track_buffer,0,lst_note[x][0],lst_note[x][1],
+                         lst_note[x][2],lst_note[x][3],lst_note[x][4])
+        f.write(track_buffer)
+    
+    #indique la fin de la track
+    f.write(struct.pack('>2h',0x00FF,0x2F00))
+    f.close()
+    
 a = [0, 36]
 b = [77760, 36]
 
-koch_values = musique(a, b, 0, 5)
-
-print(_fraction_distance(a, b, 1/2))
-print(koch_values)
-
-
-lst_note = []
-
-channel = 0x90
-volume = 0x60
-
-
-lst_note.append([0x00,channel,0x00,volume])
-
-dist_index = trunc(len(koch_values)/2)
-for x in range(dist_index-1):
-    lst_note.append([koch_values[2*x+1],channel,koch_values[2*x],volume])
-
-#Section G du track header :
-#nb de note x 4 pour le nombre de byte + 4 byte de la section F 
-track_length = len(lst_note)*4 + 4
-
-midiHeader =[0x4d, 0x54,0x68, 0x64 ,0x00, 0x00, 0x00,0x06, 0x00,
-            0x00, 0x00, 0x01, 0x00, 0x80, 0x4d, 0x54, 0x72, 0x6b]
-
-f = open('test.mid','wb')
-f.truncate()
-
-
-
-# a = [0x00, 0x01, 0x00, 0x80, 0x4D, 0x54, 0x72, 0x6B]
-# for value in a:
-#     v = bytearray(1)# buffer
-#     struct.pack_into('B', v, 0, value)
-# 
-# file.write(v)
-
-f.write(bytearray(midiHeader))
-f.write(struct.pack('>I',track_length))
-
-for x in range(len(lst_note)):
-    track_buffer= bytearray(4)
-    struct.pack_into('>4B',track_buffer,0,lst_note[x][0],lst_note[x][1],lst_note[x][2],lst_note[x][3])
-    f.write(track_buffer)
-f.write(struct.pack('>2h',0x00FF,0x2F00))
-f.close()
+__generer_midi(__musique(a, b, 0, 5))
